66c5390979ed141742b4b09c9acae9e7a2b39757
aptheparker <apapap1119@g.skku.edu>
2023-09-26 17:55:23 +0900

    fix: routes object added

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/records", (req, res) => {
-  res.render("admin/records");
+  res.render("admin/records", {
+    pageTitle: "Admin Records",
+    path: "/admin/records"
+  });
 });
 
 router.get("/add-record", (req, res) => {
--- routes/statistics.js
+++ routes/statistics.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/statistics", (req, res) => {
-  res.render("statistics/statistics");
+  res.render("statistics/statistics", {
+    pageTitle: "Statistics",
+    path: "/statistics/statistics"
+  });
 });
 
 module.exports = router;
