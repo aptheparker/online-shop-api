c9e5521dd49fb7cf4adc381cc5f1cfc684d044d7
aptheparker <apapap1119@g.skku.edu>
2023-10-21 22:15:27 +0900

    cart redirection added

--- services/CartService.js
+++ services/CartService.js
@@ -15,10 +15,11 @@ const getCartItem = async (req, res) => {
 const addCartItem = async (req, res) => {
   const { product } = req.body;
 
-  if (product) {
+  if (product) { // if product is added, then the product list will be updated.
     const productObject = JSON.parse(product);
     products.push(productObject);
-    res.redirect("/");
+    
+    res.redirect("/cart");
   } else {
     res.status(400).send("Bad Request: Missing product data");
   }
@@ -29,14 +30,13 @@ const updateCartItem = async (req, res) => {
 };
 
 const deleteCartItem = async (req, res) => {
-  const productId = req.params.id; // Corrected the parameter name
+  const productId = req.params.id;
 
   if (productId) {
     const index = products.findIndex((product) => product.id === productId);
 
     if (index !== -1) {
       products.splice(index, 1);
-      res.redirect("/");
     } else {
       res.status(404).send("Product not found");
     }
