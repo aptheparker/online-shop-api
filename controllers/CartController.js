b372774a0edba893c05eee89a9f260efb89f8c6b
aptheparker <apapap1119@g.skku.edu>
2023-11-11 17:03:01 +0900

    cart function added

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,6 +1,6 @@
 const products = [];
 
-const getCartPage = async (req, res) => {
+exports.getCartPage = async (req, res) => {
   return res.render("cart/cart", {
     pageTitle: "Cart Page",
     logoImg: "assets/jam-logo.png",
@@ -8,51 +8,46 @@ const getCartPage = async (req, res) => {
   });
 };
 
-const getCartItem = async (req, res) => {};
-
-const addCartItem = async (req, res) => {
+exports.addCartItem = async (req, res) => {
   const { product } = req.body;
   const productInJson = JSON.parse(product);
 
-  if (productInJson && products.find((product) => product.id === productInJson.id)) { // If product already exists in cart, increment quantity
+  if (
+    productInJson &&
+    products.find((product) => product.id === productInJson.id)
+  ) {
+    // If product already exists in cart, increment quantity
     const index = products.findIndex(
       (product) => product.id === productInJson.id
     );
     products[index].productQuantity++;
     res.redirect("/cart");
-  } else if (productInJson) { // If product does not exist in cart, add it
+  } else if (productInJson) {
+    // If product does not exist in cart, add it
     productInJson.productQuantity = 1;
-    products.push({...productInJson, productQuantity: 1});
+    products.push({ ...productInJson, productQuantity: 1 });
     res.redirect("/cart");
-  } else { // If product does not exist in request body, send error
+  } else {
+    // If product does not exist in request body, send error
     res.status(400).send("Bad Request: Missing product data");
   }
 };
 
-const updateCartItem = async (req, res) => {
+exports.updateCartItem = async (req, res) => {
   return;
 };
 
-const deleteCartItem = async (req, res) => {
-  const productId = req.params.id;
+exports.deleteCartItem = async (req, res) => {
+  const productId = parseInt(req.params.id);
 
   if (productId) {
     const index = products.findIndex((product) => product.id === productId);
-
+    
     if (index !== -1) {
       products.splice(index, 1);
+      res.redirect("/cart");
     } else {
       res.status(404).send("Product not found");
     }
-  } else {
-    res.status(400).send("Bad Request: Missing product data");
   }
-};
-
-module.exports = {
-  getCartPage,
-  getCartItem,
-  addCartItem,
-  deleteCartItem,
-  updateCartItem,
 };
--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const CartController = require("../controllers/CartController");
 
 router.get("/", CartController.getCartPage);
-router.get("/:id", CartController.getCartItem);
 router.post("/", CartController.addCartItem);
-router.put("/:id", CartController.updateCartItem);
+router.put("/update/:id", CartController.updateCartItem);
 router.post("/delete/:id", CartController.deleteCartItem);
 
 module.exports = router;
