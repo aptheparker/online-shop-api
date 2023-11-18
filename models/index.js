6836dce776859fc814782629982ba5b0b3d29fc5
aptheparker <apapap1119@g.skku.edu>
2023-11-18 10:05:15 +0900

    index changed

--- models/index.js
+++ models/index.js
@@ -1,21 +1,9 @@
-"use strict";
-// TODO: migration
+const { Sequelize } = require('sequelize');
+const { username, password, database, host } = require('../config/config.json')['development'];
 
-const Sequelize = require("sequelize");
-const process = require("process");
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
