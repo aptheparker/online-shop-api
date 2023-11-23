81b4e06553dd711ec64ce16db42be7f79f01b164
aptheparker <apapap1119@g.skku.edu>
2023-11-23 22:39:28 +0900

    cookie parser deleted

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-// TODO: cookie, session
-const cookieParser = require('cookie-parser');
+
 const session = require('express-session');
 app.use(session({
   secret: 'secret key',
